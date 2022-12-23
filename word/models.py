c2c60d90cc2adab797e794c2100ea9c98c0ce1aa
s1f102202516 <s1f102202516@iniad.org>
2022-12-23 17:22:28 +0900

    model仮

--- word/models.py
+++ word/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Word(models.model):
-    word = models.ChairField(max_length=999)
+    word = models.ChairField()
     meaning = models.TextField()
     test_num = models.IntegerField(default=0)
-    genre = 
-    favorite = models.IntegerField(default=0)
+    genre = models.ChairField()
+    favorite = models.IntegerField()
