48adb83d9f1bbc2c30738a861b2cc2e97babb7d2
s1f102202516 <s1f102202516@iniad.org>
2023-01-04 19:08:57 +0900

    消去

--- word/urls.py
+++ word/urls.py
@@ -5,8 +5,4 @@ urlpatterns = [
     path('', views.top, name='top'),
     path('list', views.list, name='list'),
     path('test', views.test, name='test'),
-    path('<int:article_id>/', views.detail, name='detail'),
-    path('<int:article_id>/delete', views.delete, name='delete'),
-    path('<int:article_id>/update', views.update, name='update'),
-    path('<int:article_id>/like', views.like, name='like')
 ]
